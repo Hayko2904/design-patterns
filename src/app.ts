040bb80324d90fc2bcf533fa70042490925b564e
Hayko2904 <80250629+Hayko2904@users.noreply.github.com>
2022-12-23 11:09:22 +0000

    singleton

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,8 @@ import {ProductBuilder} from "./creational-patterns/builder/product-builder";
 import { PrototypeClient } from './creational-patterns/prototype/client';
 import { Persons } from './creational-patterns/prototype/persons';
 import { Prototype } from './creational-patterns/prototype/prototype-interface';
+import { Singleton } from './creational-patterns/singleton/singleton';
+
 const app = express();
 const port = 3000;
 
@@ -17,6 +19,7 @@ app.get('/factory', (req, res) => {
   const factory = new FactoryClient();
   const productFirst = factory.runCode(new CreatorFisrt());
   const productSecond = factory.runCode(new CreatorSecond());
+  
   res.send(productFirst + '<br />' + productSecond);
 });
 
@@ -31,17 +34,27 @@ app.get('/builder', (req, res) => {
   const builder = new ProductBuilder();
   const client = new BuilderClient(builder);
   client.createProduct('product 1', 10, 15)
+
   res.send(builder.getProduct())
 })
 
 app.get('/prototype', (req, res) => {
   const personGroupFirst: Prototype = new Persons();
-  
   const personGroupSecond: Prototype = new PrototypeClient().runCode(personGroupFirst);
 
   res.send(JSON.stringify(personGroupFirst) + '<br />' + JSON.stringify(personGroupSecond));
 })
 
+app.get('/singleton', (req, res) => {
+    const singletonObjFirst = Singleton.getInstance();
+    const singletonObjSecond = Singleton.getInstance();
+
+    if (singletonObjFirst === singletonObjSecond) {
+      res.send(JSON.stringify('singletonObjFirst === singletonObjSecond'));
+    } else {
+      res.send(JSON.stringify('singletonObjFirst !== singletonObjSecond'));
+    }
+})
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
